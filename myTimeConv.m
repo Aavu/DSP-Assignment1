9098286f3d689f46800bc1225c5987a0c48cbd2a
aavu <aavu@gatech.edu>
2019-02-02 17:33:29 -0500

    mem leak fix

--- myTimeConv.m
+++ myTimeConv.m
@@ -5,8 +5,8 @@ function y = myTimeConv(x,h)
     M = length(h);
     L = N + M - 1;
     y = zeros(L, 1);
-    x = cat(1, x, sparse(M-1, 1));
-    h = cat(1, h, sparse(N-1, 1));
+    x = cat(1, x, zeros(M-1, 1));
+    h = cat(1, h, zeros(N-1, 1));
     for i=1:L 
         y(i) = dot(x(1:i),h(i-(1:i)+1)); 
     end
