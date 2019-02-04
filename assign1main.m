a69f6e9d4f537814ac9cccbf49543fc8641e95b9
aavu <aavu@gatech.edu>
2019-02-03 20:49:46 -0500

    clean up

--- assign1main.m
+++ assign1main.m
@@ -1,38 +1,46 @@
+%%
+% V. Margot Paez
+% Raghavasimhan Sankaranarayanan
+% DSP - Assignment #1
+%
+
 clc;
-clear all;
 close all;
+%% Problem 1
+% * length will be 100+200-1=299
+% * x = DC signal; y = symmetric triangle
+N = 200;
+M = 51;
+t = 0:1:N+M-2;
+x = ones(N,1);
+h = (0:1:(M-1)/2)./((M-1)/2);
+h_r = 1-h;
+h = [h h_r(2:(M+1)/2)]';
+y_time = myTimeConv(x,h);
+figure
+plot(t,y_time)
+xlabel("Time (samples)");
+ylabel("Amplitude");
+title("My Time Domain Convolution");
 
-% N = 200;
-% M = 51;
-% 
-% t = 0:1:N+M-2;
-% 
-% x = ones(N, 1);
 
-% triangular function
-% h = (0:1:(M-1)/2)./((M-1)/2);
-% h_r = 1-h;
-% h = [h h_r(2:(M+1)/2)];
-% h = transpose(h);
+%% Problem 2
+% * x = DC signal; y = symmetric triangle
+N = 200;
+M = 51;
+t = 0:1:N+M-2;
+x = ones(200,1);
+h = [linspace(0,1,26) linspace(1-1/25,0,25)]';
+y_freq = myFreqConv(x,h);
+figure
+plot(t,y_freq)
+xlabel("Time (samples)");
+ylabel("Amplitude");
+title("My Frequency Domain Convolution");
 
-% y_time = myTimeConv(x, h);
-% plot(t, y_time);
-% xlabel("Time (samples)");
-% ylabel("Amplitude");
-% title("My Time Domain Convolution");
+%% Problem 3
+x = audioread('piano.wav');
+h = audioread('impulse-response.wav');
 
-% y_freq = myFreqConv(x, h);
-% plot(t, y_freq);
-% xlabel("Time (samples)");
-% ylabel("Amplitude");
-% title("My Frequency Domain Convolution");
+[m, mabs, stdev, time] = CompareConv(x(:,1), h(:,1));
 
-x = audioread("piano.wav");
-h = audioread("impulse-response.wav");
-% y_time = myTimeConv(x, h);
-% t = 0:1:length(x)+length(h)-2;
-% plot(t, y_time);
-% xlabel("Time (samples)");
-% ylabel("Amplitude");
-% title("My Time Domain Convolution");
-[m, mabs, stdev, time] = CompareConv(x, h)
